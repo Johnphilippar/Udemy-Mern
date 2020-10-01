3376ade437aec250ee9d4eafae8640a8199b3acb
Your Name <johnphilippar@gmail.com>
2020-10-01 18:12:48 +0800

    initial commit

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAILED, LOGIN_SUCCESS, LOGOUT } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAILED, LOGIN_SUCCESS, LOGOUT, CLEAR_PROFILE } from './types';
 
 // Load User
 
@@ -89,5 +89,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout User / Clear Profile
 export const logout = () => dispatch => {
-    dispatch({ type: LOGOUT})   
+    dispatch({ type: CLEAR_PROFILE  })
+    dispatch({ type: LOGOUT  })
 }
